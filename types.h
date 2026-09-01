5894b46e689a5a6e68d822593e73ca587850bbaf
Sanju Sabu <sanjusabu.github@gmail.com>
2026-09-01 22:26:11 +0530

    Add pointer-sized integers and 128-bit integers

--- types.h
+++ types.h
@@ -2,18 +2,34 @@
 #define TYPES_H
 
 #include<stdint.h>
+#include<stddef.h>
 
 // Signed integers
 
-typedef int8_t i8;
+typedef int8_t  i8;
 typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
 
 // Unsigned integers
-typedef uint8_t u8;
+typedef uint8_t  u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64; 
+
+// Pointer-sized integers
+typedef ptrdiff_t isize;
+typedef size_t    usize;
+
+// Floats
+typedef _Float32 f32;
+typedef _Float64 f64;
+
+// 128-bit signed and unsigned integers
+#if defined(__SIZEOF_INT128__)
+    typedef __int128_t  i128;
+    typedef __uint128_t u128;
+    #define TYPES_H_HAS_128 1
+#endif
 
 #endif
